19dd8e1c41ccfb2effb9d3a6b5602e8f90b75376
cooldip83 <kuldipmathukiya2002@gmail.com>
2023-06-21 15:43:36 +0530

    testing purpose

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,16 +2,18 @@ import React from "react";
 import styles from "./Hero.module.css";
 const Hero = () => {
   return (
-    <header className={styles.hero + ' home'}>
+    <header className={styles.hero + " home"}>
       <h1>Creative</h1>
       <div>
         <h1>Digital</h1>
         <p>
-          Ayletec is an agency with hands on experience that has helped small to large projects.We also have several projects that can help your business
+          Ayletec is an agency with hands on experience that has helped small to
+          large projects.We also have several projects that can help your
+          business
         </p>
       </div>
 
-      <h1>Agency</h1>
+      <h1>company</h1>
     </header>
   );
 };
